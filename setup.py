9a139652b93a9c6293c23c8db473c58bcbf17c6f
Mcamargo85 <manuel.camargo@gmail.com>
2023-04-24 23:12:47 +0300

    feature: first commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(name='sp_model_discoverer',
+      version='1.0.12',
+      description='stochastic process model discoverer',
+      author='Manuel Camargo',
+      url='https://github.com/AdaptiveBProcess/StochasticProcessModelDiscoverer',
+      package_dir={"": "src"},
+      packages=['src'],
+      install_requires=[
+          'pandas==1.5.3',
+          'networkx==3.0',
+          'numpy==1.24.2',
+          'scikit-learn==1.2.2',
+          'hyperopt==0.2.5',
+          'jellyfish==0.9.0',
+          'opyenxes==0.3.0',
+          'pm4py==2.6.1',
+          'gensim==4.3.1',
+          'fitter==1.5.2',
+          'pyyaml==6.0',
+          'click~=8.1.3',
+          'lxml~=4.9.2',
+          'tqdm~=4.65.0']
+      )
---
+++ src/pipeline.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 13:27:58 2020
+
+@author: Manuel Camargo
+"""
+import os
+import sys
+
+import click
+import yaml
+
+from common import OUTPUT_FILES
+from common import SequencesGenerativeMethods as SqG
+from common import SplitMinerVersion as Sm
+
+
+@click.command()
+@click.option('--file', default=None, required=True, type=str)
+@click.option('--update_gen/--no-update_gen', default=False, required=False, type=bool)
+@click.option('--update_ia_gen/--no-update_ia_gen', default=False, required=False, type=bool)
+@click.option('--update_mpdf_gen/--no-update_mpdf_gen', default=False, required=False, type=bool)
+@click.option('--update_times_gen/--no-update_times_gen', default=False, required=False, type=bool)
+@click.option('--save_models/--no-save_models', default=True, required=False, type=bool)
+@click.option('--evaluate/--no-evaluate', default=True, required=False, type=bool)
+@click.option('--mining_alg', default=Sm.SM_V1, required=False, type=click.Choice(Sm().get_methods()))
+@click.option('--exp_reps', default=5, required=False, type=int)
+@click.option('--s_gen_repetitions', default=5, required=False, type=int)
+@click.option('--s_gen_max_eval', default=30, required=False, type=int)
+@click.option('--seq_gen_method', default=SqG.PROCESS_MODEL, required=False, type=click.Choice(SqG().get_methods()))
+def main(file, update_gen, update_ia_gen, update_mpdf_gen, update_times_gen, save_models, evaluate, mining_alg,
+         exp_reps, s_gen_repetitions, s_gen_max_eval, seq_gen_method):
+    params = dict()
+    params['gl'] = dict()
+    params['gl']['file'] = file
+    params['gl']['update_gen'] = update_gen
+    params['gl']['update_ia_gen'] = update_ia_gen
+    params['gl']['update_mpdf_gen'] = update_mpdf_gen
+    params['gl']['update_times_gen'] = update_times_gen
+    params['gl']['save_models'] = save_models
+    params['gl']['evaluate'] = evaluate
+    params['gl']['mining_alg'] = mining_alg
+    params = read_properties(params)
+    params['gl']['sim_metric'] = 'tsd'  # Main metric
+    # Additional metrics
+    params['gl']['add_metrics'] = ['day_hour_emd', 'log_mae', 'dl', 'mae']
+    params['gl']['exp_reps'] = exp_reps
+    # Sequences generator
+    params['s_gen'] = dict()
+    params['s_gen']['gen_method'] = seq_gen_method  # stochastic_process_model, test
+    params['s_gen']['repetitions'] = s_gen_repetitions
+    params['s_gen']['max_eval'] = s_gen_max_eval
+    params['s_gen']['concurrency'] = [0.0, 1.0]
+    params['s_gen']['epsilon'] = [0.0, 1.0]
+    params['s_gen']['eta'] = [0.0, 1.0]
+    params['s_gen']['alg_manag'] = ['replacement', 'repair']
+    params['s_gen']['gate_management'] = ['discovery', 'equiprobable']
+
+    _ensure_locations(params)
+
+    # simulator = ds.DeepSimulator(params)
+    # simulator.execute_pipeline()
+
+
+def read_properties(params):
+    """ Sets the app general params"""
+    properties_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'properties.yml')
+    with open(properties_path, 'r') as f:
+        properties = yaml.load(f, Loader=yaml.FullLoader)
+    if properties is None:
+        raise ValueError('Properties is empty')
+    paths = {k: os.path.join(*path.split('\\')) for k, path in properties.pop('paths').items()}
+    params['gl'] = {**params['gl'], **properties, **paths}
+    return params
+
+
+def _ensure_locations(params):
+    for folder in ['event_logs_path', 'bpmn_models', 'embedded_path', 'ia_gen_path', 'times_gen_path']:
+        if not os.path.exists(params['gl'][folder]):
+            os.makedirs(params['gl'][folder])
+    if not os.path.exists(OUTPUT_FILES):
+        os.makedirs(OUTPUT_FILES)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
